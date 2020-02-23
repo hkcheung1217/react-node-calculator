508ff70fdb9e8f2c17225c65cddb8e3a080958c1
Hoi <52797529+hkcheung1217@users.noreply.github.com>
2020-02-22 16:43:39 -0800

    Add files via upload

--- backend/routers/calculate.js
+++ backend/routers/calculate.js
@@ -1,1 +1,12 @@
+const express = require("express");
+const mathjs = require("mathjs");
+const router = express.Router();
 
+router.post("/", (req, res, next) => {
+  let result = req.body.result;
+  result = mathjs.evaluate(result);
+  result = mathjs.format(result, { precision: 9 });
+  res.status(200).send(result);
+});
+
+module.exports = router;
