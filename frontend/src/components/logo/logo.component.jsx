1240d9d064e6a41bd7fda00aacd11c7c85339424
Hoi <52797529+hkcheung1217@users.noreply.github.com>
2020-02-27 18:35:51 -0800

    Add files via upload

--- frontend/src/components/logo/logo.component.jsx
+++ frontend/src/components/logo/logo.component.jsx
@@ -1,1 +1,8 @@
+import React from "react";
 
+import "./logo.styles.scss";
+import mainLogo from "./../../assets/festisite_google.png";
+
+const Logo = () => <img src={mainLogo} alt="logo" />;
+
+export default Logo;
