d466f54e9103b36fae9dec4c9e255d5630cc4b16
hkcheung1217 <hoicheung@rocketmail.com>
2020-02-27 18:17:17 -0800

    Updates

---
+++ precache-manifest.c915eefe43cb7d6c82b73f7cb803af56.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4048405cfe3e58b43a98e68db7f324b1",
+    "url": "/react-node-calculator/index.html"
+  },
+  {
+    "revision": "deae0f6da3e46e8ef3fe",
+    "url": "/react-node-calculator/static/css/main.d5aeae61.chunk.css"
+  },
+  {
+    "revision": "cd646393b5f8a921bd70",
+    "url": "/react-node-calculator/static/js/2.2ce67aa1.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/react-node-calculator/static/js/2.2ce67aa1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "deae0f6da3e46e8ef3fe",
+    "url": "/react-node-calculator/static/js/main.4213e506.chunk.js"
+  },
+  {
+    "revision": "951d30ed9227733090b6",
+    "url": "/react-node-calculator/static/js/runtime-main.0e4153d6.js"
+  }
+]);
