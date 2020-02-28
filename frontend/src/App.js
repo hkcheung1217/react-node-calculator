855adc3492e51a888f92142d5bca66708a58ea45
Hoi <52797529+hkcheung1217@users.noreply.github.com>
2020-02-27 18:30:51 -0800

    Add files via upload

---
+++ frontend/src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./App.css";
+
+import Logo from "./components/logo/logo.component";
+import Calculator from "./components/calculator/calculator.component";
+
+const App = () => (
+  <div className="app">
+    <Logo />
+    <Calculator />
+  </div>
+);
+
+export default App;
